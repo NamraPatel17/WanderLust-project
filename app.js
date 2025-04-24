d7a185844dfb911ce7b08980527074e705a4a3ee
Namra Patel <namrapatel910@gmail.com>
2025-04-24 18:38:09 +0530

    CREATE(New & Create Route)

--- app.js
+++ app.js
@@ -34,6 +34,11 @@ app.get("/listings", async (req,res) => {
   res.render("listings/index.ejs", {allListings});
 });
 
+//New Route
+app.get("/listings/new",(req,res) => {
+  res.render("listings/new.ejs");
+});
+
 //Show Route
 app.get("/listings/:id",async (req,res)=> {
   let {id} = req.params;
@@ -41,8 +46,13 @@ app.get("/listings/:id",async (req,res)=> {
   res.render("listings/show.ejs",{listing});
 });
 
-
-
+//Create Route
+app.post("/listings", async (req,res) => {
+  //let {title, description, image, price, country, location} = req.body;
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
 
 // app.get("/testListing", async (req,res) => {
 //   let sampleListing = new Listing({
