90ad93484bb1dd8874aa239ba14934e9f9d1df16
Namra Patel <namrapatel910@gmail.com>
2025-05-01 18:38:48 +0530

    Validation for Schema

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -34,6 +35,16 @@ app.get("/",(req,res) => {
   res.send("Hi , I am root");
 });
 
+const validateListing = (req, res, next)=> {
+  let {error} = listingSchema.validate(req.body);
+  if(error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
 //Index route
 app.get("/listings", wrapAsync(async (req,res) => {
   const allListings = await Listing.find({});
@@ -53,10 +64,7 @@ app.get("/listings/:id", wrapAsync(async (req,res)=> {
 }));
 
 //Create Route
-app.post("/listings", wrapAsync(async (req,res, next) => {
-  if(!req.body.listing) {
-    throw new ExpressError(400, "Send Valid Data for Listing")
-  }
+app.post("/listings", validateListing, wrapAsync(async (req,res, next) => {
   const newListing = new Listing(req.body.listing);
   await newListing.save();
   res.redirect("/listings");
@@ -70,10 +78,7 @@ app.get("/listings/:id/edit", wrapAsync(async (req,res)=> {
 }));
 
 //Update Route
-app.put("/listings/:id", wrapAsync(async (req,res)=> {
-  if(!req.body.listing) {
-    throw new ExpressError(400, "Send Valid Data for Listing")
-  }
+app.put("/listings/:id", validateListing, wrapAsync(async (req,res)=> {
   let {id} = req.params;
   await Listing.findByIdAndUpdate(id,{...req.body.listing});
   res.redirect(`/listings/${id}`);
@@ -104,9 +109,9 @@ app.delete("/listings/:id", wrapAsync(async (req,res)=> {
 //   res.send("successful testing");
 // });
 
-// app.all("*", (req, res, next) => {
-//   next(new ExpressError(404, "Page Not Found!"));
-// });
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found!"));
+});
 
 app.use((err, req, res, next) => {
   let { statusCode=500, message="Something Went Wrong"} = err;
