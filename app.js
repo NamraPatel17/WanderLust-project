33fdb51605ccc4e148b007a0880c214ee3f27884
Namra Patel <namrapatel910@gmail.com>
2025-05-04 19:28:08 +0530

    some typo error

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ app.post("/listings/:id/reviews", validateReview,  wrapAsync(async(req,res) => {
 app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
   let {id, reviewId} = req.params;
 
-  await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewsId}});
+  await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
   await Review.findByIdAndDelete(reviewId);
 
   res.redirect(`/listings/${id}`);
