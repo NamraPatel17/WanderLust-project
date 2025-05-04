c5bebc9ef654718ca39145dcd0905ba48dec4a72
Namra Patel <namrapatel910@gmail.com>
2025-05-04 19:24:36 +0530

    Deleting Reviews

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.delete("/listings/:id", wrapAsync(async (req,res)=> {
 
 
 //Review 
-//Post Route
+//Post Review Route
 app.post("/listings/:id/reviews", validateReview,  wrapAsync(async(req,res) => {
   let listing= await Listing.findById(req.params.id);
   let newReview = new Review(req.body.review);
@@ -119,8 +119,15 @@ app.post("/listings/:id/reviews", validateReview,  wrapAsync(async(req,res) => {
   res.redirect(`/listings/${listing._id}`);
 }));
 
+//Delete Review Route
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
+  let {id, reviewId} = req.params;
 
+  await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewsId}});
+  await Review.findByIdAndDelete(reviewId);
 
+  res.redirect(`/listings/${id}`);
+}));
 
 // app.get("/testListing", async (req,res) => {
 //   let sampleListing = new Listing({
