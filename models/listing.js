588a9250f8115d2c9f604a2fbe619243a74e104f
Namra Patel <namrapatel910@gmail.com>
2025-05-04 19:39:49 +0530

    Handling Delete Listing

--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const { stringify } = require("querystring");
-const review = require("./review");
+const Review = require("./review.js");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -23,6 +23,12 @@ const listingSchema = new Schema({
     },
   ],
 });
+
+listingSchema.post("findOneAndDelete", async(listing) => {
+  if(listing) {
+    await Review.deleteMany({_id: {$in: listing.reviews}});
+  }
+})
 
 const Listing = mongoose.model("listing", listingSchema);
 module.exports  = Listing;
