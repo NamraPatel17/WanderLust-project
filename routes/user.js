a8ef8799080a87f36a15d6668964cd0b91bf5a42
Namra Patel <namrapatel910@gmail.com>
2025-06-01 12:03:12 +0530

    Created Login Page

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
+const { render } = require("ejs");
+const passport = require("passport");
 
 router.get("/signup", (req, res) =>{
   res.render("users/signup.ejs");
@@ -19,7 +21,15 @@ router.post("/signup", wrapAsync(async (req, res) => {
     req.flash("error", e.messge);
     res.redirect("/signup");
   }
-  
 }));
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post("/login" , passport.authenticate("local", {failureRedirect: '/login' , failureFlash: true }) ,async(req, res) => {
+  res.flash("success", "Welcome back to Wanderlust!");
+  res.redirect("/listings");
+})
 
 module.exports = router;
