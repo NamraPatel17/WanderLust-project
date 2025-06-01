f066ea6a5db448498d3b4e10e910b214624fd9b6
Namra Patel <namrapatel910@gmail.com>
2025-06-01 11:22:02 +0530

    configuring strategy

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+const { parentPort } = require("worker_threads");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -47,6 +51,14 @@ app.get("/",(req, res) => {
 
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
