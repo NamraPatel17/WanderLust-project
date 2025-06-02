0bab6acfa6222ff3944326e6f2d289fadc4ed965
Namra Patel <namrapatel910@gmail.com>
2025-06-02 10:44:40 +0530

    Connecting Login Route

---
+++ middleware.js
@@ -0,0 +1,9 @@
+const { request } = require("express");
+
+module.exports.isLoggedIn = (req, res, next) => {
+  if(!req.isAuthenticated()) {
+    req.flash("error", "You must be logged in to create listing!");
+    return res.redirect("/login");
+  }
+  next();
+}
