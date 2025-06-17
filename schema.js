a26be30c023714c4c196d2f549d76e7a20a8373d
Namra Patel <namrapatel910@gmail.com>
2025-06-17 15:39:55 +0530

    valid error

--- schema.js
+++ schema.js
@@ -7,10 +7,12 @@ module.exports.listingSchema = Joi.object({
     location:Joi.string().required(),
     country:Joi.string().required(),
     price: Joi.number().required().min(0),
-    image: Joi.object({url:Joi.string(),filename:Joi.string()})
+    image: Joi.object({
+      url: Joi.string().required(),
+      filename: Joi.string(),
+    }), 
   }).required(),
 });
-
 
 module.exports.reviewSchema = Joi.object({
   review:Joi.object({
