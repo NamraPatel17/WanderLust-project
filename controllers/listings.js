777d17cea5b8157d4c443ed4511943ab1505b502
Namra Patel <namrapatel910@gmail.com>
2025-06-17 14:55:09 +0530

    done

--- controllers/listings.js
+++ controllers/listings.js
@@ -24,7 +24,7 @@ module.exports.createListing = async (req,res, next) => {
   let filename = req.file.filename;
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
-  newListing.image= {url, filename};
+  newListing.image = req.files.map(f => ({ url: f.path, filename: f.filename }));
   await newListing.save();
   req.flash("success", "New Listing Created!");
   res.redirect("/listings");
